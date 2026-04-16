98e89bc2af7f42a5f7c6fcbc48551326fd20f324
Isaac <ironisaac2004@gmail.com>
2026-04-16 10:22:46 -0600

    [UPDATE]: Se actualizaron las paginas para que sirvan

---
+++ assets/js/register.js
@@ -0,0 +1,23 @@
+document.getElementById('registerForm')?.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const name = document.getElementById('name').value
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    const confirmPassword = document.getElementById('confirmPassword').value
+
+    if (!name || !email || !password || !confirmPassword) {
+        showAlert('registerAlert', 'Todos los daots son obligatorios')
+        return
+    }
+    if (password !== confirmPassword) {
+        showAlert('registerAlert', 'Las contraseñas no son iguales')
+        return
+    }
+
+    //Simulacion de registro
+    localStorage.setItem('userName', name)
+
+    showAlert('registerAlert', 'Registro exitoso')
+    window.location.href = 'login.html'
+})
